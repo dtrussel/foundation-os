109ff5e14bec161acd0addb402dcffc743c95c2f
David Trussel <d.crucel@gmail.com>
2020-10-27 14:40:34 +0100

    Add dockerfile for build

--- setup.sh
+++ setup.sh
@@ -3,12 +3,8 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname $SCRIPT)
 
-sudo apt-get install gawk wget git-core diffstat \
-  unzip texinfo gcc-multilib build-essential chrpath socat
-
 git submodule update --init --recursive
 source ${SCRIPT_DIR}/poky/oe-init-build-env ${SCRIPT_DIR}/build
 
-bitbake-layers add-layer ${SCRIPT_DIR}/meta-openembedded
+bitbake-layers add-layer ${SCRIPT_DIR}/meta-openembedded/meta-oe
 bitbake-layers add-layer ${SCRIPT_DIR}/meta-foundation
-
